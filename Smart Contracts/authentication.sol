7621e21656ae02f2a7e8bb37c0619369fad0db83
Jack Langdon <137903884+jacklangy@users.noreply.github.com>
2026-05-24 15:50:20 +1000

    Override Force Push to authentication.sol

--- Smart Contracts/authentication.sol
+++ Smart Contracts/authentication.sol
@@ -7,11 +7,17 @@ contract authentication {
     mapping(address => bool) public isRegulator;
     mapping(address => bool) public isMember;
 
+    // Tracks addresses that have applied but not yet been approved
+    mapping(address => bool) public hasPendingApplication;
+    address[] public applicantList;
+
     // Events for transparency
     event regulatorAdded(address indexed account, address indexed addedBy);
     event regulatorRemoved(address indexed account, address indexed removedBy);
     event MemberPromoted(address indexed account, address indexed promotedBy);
     event MemberDemoted(address indexed account, address indexed demotedBy);
+    event ApplicationSubmitted(address indexed applicant);
+    event ApplicationRejected(address indexed applicant, address indexed rejectedBy);
 
 
     modifier onlyRegulator() {
@@ -25,7 +31,47 @@ contract authentication {
         emit regulatorAdded(msg.sender, address(0));
     }
 
-    // --- Admin Management Functions ---
+    // Anyone can call this to submit a membership application
+    function applyForMembership() external {
+        require(!isMember[msg.sender], "Already a member");
+        require(!isRegulator[msg.sender], "Regulators cannot apply");
+        require(!hasPendingApplication[msg.sender], "Application already pending");
+
+        hasPendingApplication[msg.sender] = true;
+        applicantList.push(msg.sender);
+
+        emit ApplicationSubmitted(msg.sender);
+    }
+
+    // Returns all pending applicant addresses
+    function getApplicants() external view returns (address[] memory) {
+        return applicantList;
+    }
+
+    // Regulator: Approve / Reject
+
+    function approveApplicant(address _applicant) external onlyRegulator {
+        require(hasPendingApplication[_applicant], "No pending application");
+
+        hasPendingApplication[_applicant] = false;
+        _removeFromApplicantList(_applicant);
+
+        isMember[_applicant] = true;
+        emit MemberPromoted(_applicant, msg.sender);
+    }
+
+    function rejectApplicant(address _applicant) external onlyRegulator {
+        require(hasPendingApplication[_applicant], "No pending application");
+
+        hasPendingApplication[_applicant] = false;
+        _removeFromApplicantList(_applicant);
+
+        emit ApplicationRejected(_applicant, msg.sender);
+    }
+
+
+
+    // Admin Management Functions
 
 
     function addRegulator(address _newRegulator) external onlyRegulator {
@@ -45,7 +91,7 @@ contract authentication {
         emit regulatorRemoved(_regulatorToRemove, msg.sender);
     }
 
-    // --- Membership Management Functions ---
+    // Membership Management Functions
 
 
     function promoteToMember(address _account) external onlyRegulator {
@@ -67,5 +113,17 @@ contract authentication {
     function memberAction() external view returns (string memory) {
         require(isMember[msg.sender], "Must be a member to call this");
         return "Welcome to the inner circle.";
+    }
+
+    // Internal Helper
+
+    function _removeFromApplicantList(address _applicant) internal {
+        for (uint i = 0; i < applicantList.length; i++) {
+            if (applicantList[i] == _applicant) {
+                applicantList[i] = applicantList[applicantList.length - 1];
+                applicantList.pop();
+                break;
+            }
+        }
     }
 }
